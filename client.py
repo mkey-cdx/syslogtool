1097834fdbe3239062cf8ed5be79d713acd1dae8
Mickaël Coiraton <mkey-cdx@users.noreply.github.com>
2017-09-08 17:39:59 +0200

    Add files via upload

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import socket
 import os
 import configparser
 
-# Get config data
+# Get config data.
 config = configparser.ConfigParser()
 config.read("client.ini")
 
@@ -15,21 +15,24 @@ file_name = config.get("Config", "file_name")
 user = os.environ['USER']
 
 
-# Initialize client socket
+# Initialize client socket.
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect((host, port))
 print("Connected on {}".format(port))
 
-# Send GET_LOG request
+# Send GET_LOG request.
 socket.send(b'GET_LOG')
 
-# Get server response
+# Get server response.
 response = socket.recv(255)
 
 if (response == b'OK'):
+    # File provided. Execute rsync to get it.
     print("File provided on server. Downloading...")
     os.system(str.format("rsync -vz -e ssh {0}@{1}:{2}{3} .", user, host, file_path, file_name))
     socket.send(b'OK')
+elif (response == b'KO'):
+    print("ERROR: file not found. Check .ini file")
 
 socket.close()
 print("Connection closed")
